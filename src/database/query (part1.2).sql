3322e222497065ff8d3522488012d6c75d2a89f0
Konrad Kucab <karab471@gmail.com>
2025-05-11 18:00:45 +0200

    Task

---
+++ src/database/query (part1.2).sql
@@ -0,0 +1,19 @@
+SELECT 
+    p.product_id AS productId,
+    SUM(p.weight * op.quantity) AS totalWeight
+FROM 
+    orders o
+JOIN 
+    orders_products op ON o.order_id = op.order_id
+JOIN 
+    products p ON op.product_id = p.product_id
+JOIN 
+    route_segments rs ON o.order_id = rs.order_id
+WHERE 
+    o.customer_id = 32
+    AND rs.segment_type = 'STOP'
+    AND DATE(rs.segment_end_time) = '2024-02-13'
+GROUP BY 
+    p.product_id
+ORDER BY 
+    totalWeight ASC;
